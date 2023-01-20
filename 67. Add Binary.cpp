502b46a5f304cf2b64f8a0995a66c0954d6eb255
ShreyaUwU <107616835+ShreyaUwU@users.noreply.github.com>
2023-01-20 13:41:43 +0530

    Create 67. Add Binary.cpp

---
+++ 67. Add Binary.cpp
@@ -0,0 +1,25 @@
+//leetcode 67
+//reference: https://leetcode.com/problems/add-binary/description/
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int i= a.size()-1;
+        int j= b.size()-1;
+        int carry = 0;
+        string res;
+
+        while(i>=0 || j>=0){
+            int sum = carry;
+            if(i>=0)sum+=a[i]-'0';
+            if(j>=0)sum+=b[j]-'0';
+            carry = sum>1 ? 1 : 0;
+            res+= to_string(sum%2);
+            i--;
+            j--;
+        }
+        if(carry)res+= to_string(carry);
+        reverse(res.begin(),res.end());
+        return res;
+        
+    }
+};
