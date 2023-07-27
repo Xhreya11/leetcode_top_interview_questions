5d6242c5eb6492feb383685b3cc7442627fba413
Xhreya11 <89789614+Xhreya11@users.noreply.github.com>
2023-07-27 19:43:10 +0530

    Time: 3 ms (35.37%), Space: 6.1 MB (36.37%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    int f(int n ,vector<int> &dp){
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1, -1);
+        return f(n,dp);
+    }
+};
